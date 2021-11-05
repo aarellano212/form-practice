00f491085065b72c17721c01f01a22e424ce844d
wk26 <wk26@fresnou.org>
2021-11-05 13:19:56 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.post('/results', function (req, res) {
+    res.render('result', {
+        description: req.body.description,
+        agree: req.body.agree,
+        animals: req.body.animals,
+        animalsOther: req.body.animalsOther,
+    })
+
+})
+
 
 module.exports = router;
